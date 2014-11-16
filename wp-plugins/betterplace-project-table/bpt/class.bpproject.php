44cc1845ff4f4ef194dac54af001d078aa6d940d
andibraeu <ab@andi95.de>
2014-11-16 18:26:27 +0100

    initial ffapi classes

---
+++ wp-plugins/betterplace-project-table/bpt/class.bpproject.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andi
+ * Date: 16.11.14
+ * Time: 18:21
+ */
+
+class bpproject {
+
+} 
---
+++ wp-plugins/betterplace-project-table/bpt/class.ffapi.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andi
+ * Date: 16.11.14
+ * Time: 18:16
+ */
+
+class ffapi {
+
+} 
