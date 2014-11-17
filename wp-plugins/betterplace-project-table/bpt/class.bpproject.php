2ccab238ec948d9a04fea5fd36aa530f1d0a8024
andibraeu <ab@andi95.de>
2014-11-17 21:15:37 +0100

    fetch betterplace projects

--- wp-plugins/betterplace-project-table/bpt/class.bpproject.php
+++ wp-plugins/betterplace-project-table/bpt/class.bpproject.php
@@ -6,6 +6,59 @@
  * Time: 18:21
  */
 
-class bpproject {
+class bpProject {
 
+    const bpApiUrl = "https://api.betterplace.org/de/api_v4/projects/";
+    private $campaignId;
+    private $projectLink;
+    private $donationLink;
+    private $projectImage;
+    private $projectTitle;
+    private $openAmount;
+    private $incompleteNeed;
+    private $progress;
+    private $donors;function
+
+    __construct($campaignId)
+    {
+        $this->campaignId = $campaignId;
+        $this->getProjectDetails();
+    }
+
+    public function getProjectDetails() {
+        $prjDetails = file_get_contents(self::bpApiUrl . $this->campaignId . ".json");
+        $prjDetailsJson = json_decode($prjDetails, true);
+        foreach($prjDetailsJson['links'] as $links) {
+            if ($links['rel'] == 'platform') {
+                $this->projectLink = $links['href'];
+            } elseif ($links['rel'] == 'new_donation' ) {
+                $this->donationLink = $links['href'];
+            }
+        }
+        foreach($prjDetailsJson['profile_picture']['links'] as $pic) {
+            if ($pic['rel'] == 'fill_270x141') {
+                $this->projectImage = $pic['href'];
+            }
+        }
+
+        $this->projectTitle = $prjDetailsJson['title'];
+        $this->openAmount = $prjDetailsJson['open_amount_in_cents'];
+        $this->incompleteNeed = $prjDetailsJson['incomplete_need_count'];
+        $this->progress = $prjDetailsJson['progress_percentage'];
+        $this->donors = $prjDetailsJson['donor_count'];
+        return $this;
+    }
+
+    public function getProjectArray() {
+        $return = array();
+        $return['projectLink'] = $this->projectLink;
+        $return['donationLink'] = $this->donationLink;
+        $return['projectImage'] = $this->projectImage;
+        $return['projectTitle'] = $this->projectTitle;
+        $return['openAmount'] = $this->openAmount;
+        $return['incompleteNeed'] = $this->incompleteNeed;
+        $return['progress'] = $this->progress;
+        $return['donors'] = $this->donors;
+        return $return;
+    }
 } 
---
+++ wp-plugins/betterplace-project-table/bpt/tests/bpProjectTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andi
+ * Date: 17.11.14
+ * Time: 20:49
+ */
+
+include_once("class.bpproject.php");
+
+class bpProjectTest extends PHPUnit_Framework_TestCase {
+
+    private $project;
+
+    function __construct()
+    {
+        $this->project = new bpProject("14895");
+    }
+
+    public function testCampaign()
+    {
+        $result = $this->project->getProjectDetails()->getProjectArray();
+        $this->assertEquals("https://www.betterplace.org/de/projects/14895-weimarnetz-e-v-freies-wlan-in-weimar", $result['projectLink']);
+        $this->assertEquals("https://asset1.betterplace.org/uploads/project/profile_picture/000/014/895/fill_270x141_WCW2014.jpg", $result['projectImage']);
+        $this->assertEquals("https://www.betterplace.org/de/projects/14895/donations/new", $result['donationLink']);
+    }
+
+
+}
+ 
