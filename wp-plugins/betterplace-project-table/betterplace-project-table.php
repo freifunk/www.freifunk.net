3714ff9e6a138b593bcd19feff349b516f56453b
andibraeu <ab@andi95.de>
2015-07-13 14:52:00 +0200

    bpt: check if fields are empty to prevent nonsense output

--- wp-plugins/betterplace-project-table/betterplace-project-table.php
+++ wp-plugins/betterplace-project-table/betterplace-project-table.php
@@ -15,15 +15,16 @@ include_once("bpt/class.ffapi.php");
 include_once("bpt/class.bpproject.php");
 include_once("bpt/class.boostCampaign.php");
 
-function betterplaceprojecttable($atts) {
+function betterplaceprojecttable($atts)
+{
     $use_ffapi = null;
-    $a = shortcode_atts( array(
+    $a = shortcode_atts(array(
         'orderBy' => 'openAmount',
         'sort' => 'desc',
         'use_ffapi' => 'true',
         'show_provider' => 'true',
         'more_campaigns' => null
-    ), $atts ) ;
+    ), $atts);
 
     $orderBy = $a['orderBy'];
     $sort = $a['sort'];
@@ -41,77 +42,77 @@ function betterplaceprojecttable($atts) {
     $bpProjects = array();
     $output = "";
 
-    if (! empty($more_campaigns)) {
+    if (!empty($more_campaigns)) {
         $additionalCampaigns = explode(",", $more_campaigns);
-        foreach($additionalCampaigns as $number=>$ac) {
-            array_push($campaigns, array( $number => array("projectid"=>$ac, "provider" => "betterplace")));
+        foreach ($additionalCampaigns as $number => $ac) {
+            array_push($campaigns, array("projectid" => $ac, "provider" => "betterplace"));
         }
     }
 
     $campaigns = array_unique($campaigns, SORT_REGULAR);
-    foreach($campaigns as $name => $projects) {
-        foreach ($projects as $project) {
-            if ( false === ( $bp = get_transient( $project['provider'].$project['projectid'] ) ) ) {
-                $bp = $df->getDonationClass($project['provider'], $project['projectid'], $name);
-                set_transient( $project['provider'].$project['projectid'], $bp, get_option('cache_timeout') );
-            }
-            if (is_object($bp)) {
-                array_push($bpProjects, $bp->getProjectArray());
-            }
+
+    foreach ($campaigns as $name => $project) {
+        if (false === ($bp = get_transient($project['provider'] . $project['projectid']))) {
+            $bp = $df->getDonationClass($project['provider'], $project['projectid'], $name);
+            set_transient($project['provider'] . $project['projectid'], $bp, get_option('cache_timeout'));
+        }
+        if (is_object($bp)) {
+            array_push($bpProjects, $bp->getProjectArray());
         }
     }
 
-
-    usort($bpProjects, function($a, $b) use ($orderBy) {
+    usort($bpProjects, function ($a, $b) use ($orderBy) {
         return $a[$orderBy] - $b[$orderBy];
     });
-    if ( ! empty($sort) && $sort == "desc") {
+
+    if (!empty($sort) && $sort == "desc") {
         $bpProjects = array_reverse($bpProjects);
     }
-    wp_enqueue_script( 'sortable', get_template_directory_uri() . '/js/sorttable.js', array(), null, false );
+    wp_enqueue_script('sortable', get_template_directory_uri() . '/js/sorttable.js', array(), null, false);
 
     $output .= "<div class=\"betterplace-table\">";
     $output .= "<table class=\"sortable betterplace-table\">";
     $output .= "<thead>";
-    $output .= "<th class=\"" . getSortedClass($orderBy, "projectTitle") ."\">Projekt";
-    if ( $show_provider === 'true') $output .= "/Träger";
-    $output .= getSortSign($orderBy, "projectTitle") ."</th>";
-    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "incompleteNeed") . "\">".__('Bedarfe', 'bpt') . getSortSign($orderBy, "incompleteNeed") ." </th>";
-    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "completedNeed") . "\">Erfüllt" . getSortSign($orderBy, "completedNeed") ." </th>";
-    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "donors") . "\">Spender" . getSortSign($orderBy, "donors") ." </th>";
-    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "progress") . "\">Fortschritt" . getSortSign($orderBy, "progress") ." </th>";
-    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "openAmount") . "\">Spenden" . getSortSign($orderBy, "openAmount")  ." </th>";
+    $output .= "<th class=\"" . getSortedClass($orderBy, "projectTitle") . "\">Projekt";
+    if ($show_provider === 'true') $output .= "/Träger";
+    $output .= getSortSign($orderBy, "projectTitle") . "</th>";
+    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "incompleteNeed") . "\">" . __('Bedarfe', 'bpt') . getSortSign($orderBy, "incompleteNeed") . " </th>";
+    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "completedNeed") . "\">Erfüllt" . getSortSign($orderBy, "completedNeed") . " </th>";
+    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "donors") . "\">Spender" . getSortSign($orderBy, "donors") . " </th>";
+    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "progress") . "\">Fortschritt" . getSortSign($orderBy, "progress") . " </th>";
+    $output .= "<th class=\"sorttable_numeric " . getSortedClass($orderBy, "openAmount") . "\">Spenden" . getSortSign($orderBy, "openAmount") . " </th>";
     $output .= "</thead>";
 
-  foreach($bpProjects as $bpProject) {
-    $output .= "<tr>";
-    $output .= "<td class=\"organization\">".$bpProject['projectTitle'];
-    if ( $show_provider === 'true' && ! empty($bpProject['organization'])) $output .= "<br/><a href=\"#". $bpProject['organization'] ."\">" . $bpProject['organization'] . "</a>";
-    $output .= "</td>";
-    $output .= "<td class=\"numeric\">";
-    if (! empty($bpProject['incompleteNeed'])) $output .=  $bpProject['incompleteNeed'];
-    $output .= "</td>";
-    $output .= "<td class=\"numeric\">" . $bpProject['completedNeed'] . "</td>";
-    $output .= "<td class=\"numeric\">" . $bpProject['donors'] . "</td>";
-    if (empty($bpProject['progress'])) {
-        $output .= "<td></td>";
-    }  else {
-        $output .= "<td class=\"progress\" sorttable_customkey='" . $bpProject['progress'] . "'>" . do_shortcode("[wppb progress=" . $bpProject['progress'] . " fullwidth=false option=flat location=inside color=#009ee0]") . "</td>";
+    foreach ($bpProjects as $bpProject) {
+        $output .= "<tr>";
+        $output .= "<td class=\"organization\">" . $bpProject['projectTitle'];
+        if ($show_provider === 'true' && !empty($bpProject['organization'])) $output .= "<br/><a href=\"#" . $bpProject['organization'] . "\">" . $bpProject['organization'] . "</a>";
+        $output .= "</td>";
+        $output .= "<td class=\"numeric\">";
+        if (!empty($bpProject['incompleteNeed'])) $output .= $bpProject['incompleteNeed'];
+        $output .= "</td>";
+        $output .= "<td class=\"numeric\">" . $bpProject['completedNeed'] . "</td>";
+        $output .= "<td class=\"numeric\">" . $bpProject['donors'] . "</td>";
+        if (empty($bpProject['progress'])) {
+            $output .= "<td></td>";
+        } else {
+            $output .= "<td class=\"progress\" sorttable_customkey='" . $bpProject['progress'] . "'>" . do_shortcode("[wppb progress=" . $bpProject['progress'] . " fullwidth=false option=flat location=inside color=#009ee0]") . "</td>";
+        }
+        if (empty($bpProject['openAmount']) && !empty($bpProject['totalAmount'])) {
+            $output .= "<td class=\"donor\" sorttable_customkey='0'>Schon " . round($bpProject['totalAmount'] / 100) . " € sind da<a href=\"" . $bpProject['projectLink'] . "\" target=\"_blank\"><button>Jetzt spenden!</button></a></td>";
+        } else {
+            $output .= "<td class=\"donor\" sorttable_customkey='" . $bpProject['openAmount'] . "'>Es fehlen noch " . round($bpProject['openAmount'] / 100) . " €<a href=\"" . $bpProject['projectLink'] . "\" target=\"_blank\"><button>Jetzt spenden!</button></a></td>";
+        }
+        $output .= "</tr>";
     }
-    if (empty($bpProject['openAmount']) && ! empty($bpProject['totalAmount'])) {
-        $output .= "<td class=\"donor\" sorttable_customkey='0'>Schon " . round($bpProject['totalAmount'] / 100) . " € sind da<a href=\"". $bpProject['projectLink']  ."\" target=\"_blank\"><button>Jetzt spenden!</button></a></td>";
-    } else {
-        $output .= "<td class=\"donor\" sorttable_customkey='" . $bpProject['openAmount'] . "'>Es fehlen noch " . round($bpProject['openAmount'] / 100) . " €<a href=\"" . $bpProject['projectLink'] . "\" target=\"_blank\"><button>Jetzt spenden!</button></a></td>";
-    }
-    $output .= "</tr>";
-  }
 
-$output .= "</table>";
-$output .= "</div>";
-return $output;
+    $output .= "</table>";
+    $output .= "</div>";
+    return $output;
 }
 
-function getSortSign($orderBy, $column) {
+function getSortSign($orderBy, $column)
+{
     if ($orderBy == $column) {
         return "<span id='sorttable_sortfwdind'>&nbsp;▾</span>";
     } else {
@@ -119,15 +120,14 @@ function getSortSign($orderBy, $column) {
     }
 }
 
-function getSortedClass($orderBy, $column) {
+function getSortedClass($orderBy, $column)
+{
     if ($orderBy == $column) {
         return "sorttable_sorted";
     } else {
         return "";
     }
 }
-
-
 
 
 add_option('ffapi_summarized_dir', "http://freifunk.net/map/ffSummarizedDir.json");
--- wp-plugins/betterplace-project-table/bpt/class.ffapi.php
+++ wp-plugins/betterplace-project-table/bpt/class.ffapi.php
@@ -1,55 +1,59 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: andi
  * Date: 16.11.14
  * Time: 18:16
  */
+class ffapi
+{
 
-class ffapi {
+    private $summarizedApiUrl;
+    private $summarizedApi;
 
-	private $summarizedApiUrl;
-	private $summarizedApi;
-
-    private function isAssoc($arr){
+    private function isAssoc($arr)
+    {
         return array_keys($arr) !== range(0, count($arr) - 1);
     }
 
-	function __construct( $summarizedApiUrl ) {
-        $ctx = stream_context_create(array('http'=>
+    function __construct($summarizedApiUrl)
+    {
+        $ctx = stream_context_create(array('http' =>
             array(
                 'timeout' => get_option('http_timeout'), // 1 200 Seconds = 20 Minutes
             )
         ));
         $this->summarizedApiUrl = $summarizedApiUrl;
-		$rawFile = file_get_contents($this->getSummarizedApiUrl(), null, $ctx);
-		$json = json_decode($rawFile, true);
-		$this->summarizedApi = $json;
-	}
+        $rawFile = file_get_contents($this->getSummarizedApiUrl(), null, $ctx);
+        $json = json_decode($rawFile, true);
+        $this->summarizedApi = $json;
+    }
 
 
-	/**
-	 * @param $path
-	 * @return array
-	 *
-	 * return values for a path. the path should separate by points
-	 */
-	public function getValues($path) {
+    /**
+     * @param $path
+     * @return array
+     *
+     * return values for a path. the path should separate by points
+     */
+    public function getValues($path)
+    {
         $result = array();
-        foreach($this->summarizedApi as $name => $community) {
+        foreach ($this->summarizedApi as $name => $community) {
             $values = $this->getValuesFromCommunity($community, $path);
-            if ( ! empty($values) && count($values) == 1 ) {
-                $result[$name] = $values;
-            } elseif (! empty($values) && count($values) > 1) {
+            if (!empty($values) && count($values) == 1) {
+                $result[$name] = $values[0];
+            } elseif (!empty($values) && count($values) > 1) {
                 $valueCounter = 1;
-                foreach($values as $singleValue) {
-                    $result[$name.$valueCounter] = $singleValue;
+                foreach ($values as $singleValue) {
+                    $result[$name . $valueCounter] = $singleValue;
                     $valueCounter++;
                 }
             }
         }
         return $result;
-	}
+    }
 
     /**
      * @param $community
@@ -68,46 +72,50 @@ class ffapi {
                     $result = $this->getValuesFromCommunity($community[$element], implode(".", $pathArray));
                 } else {
                     $result = array();
-                    foreach($community[$element] as $cElement) {
+                    foreach ($community[$element] as $cElement) {
                         array_push($result, $this->getValuesFromCommunity($cElement, implode(".", $pathArray)));
                     }
                 }
             }
         } else {
-            if (! empty($community[$pathArray[0]])) {
+            if (!empty($community[$pathArray[0]])) {
                 $result = $community[$pathArray[0]];
             }
         }
         return $result;
     }
 
-	/**
-	 * @return mixed
-	 */
-	public function getSummarizedApi() {
-		return $this->summarizedApi;
-	}
+    /**
+     * @return mixed
+     */
+    public function getSummarizedApi()
+    {
+        return $this->summarizedApi;
+    }
 
-	/**
-	 * @param mixed $summarizedApi
-	 */
-	public function setSummarizedApi( $summarizedApi ) {
-		$this->summarizedApi = $summarizedApi;
-	}
+    /**
+     * @param mixed $summarizedApi
+     */
+    public function setSummarizedApi($summarizedApi)
+    {
+        $this->summarizedApi = $summarizedApi;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getSummarizedApiUrl() {
-		return $this->summarizedApiUrl;
-	}
+    /**
+     * @return mixed
+     */
+    public function getSummarizedApiUrl()
+    {
+        return $this->summarizedApiUrl;
+    }
 
-	/**
-	 * @param mixed $summarizedApiUrl
-	 */
-	public function setSummarizedApiUrl( $summarizedApiUrl ) {
-		$this->summarizedApiUrl = $summarizedApiUrl;
-	}
+    /**
+     * @param mixed $summarizedApiUrl
+     */
+    public function setSummarizedApiUrl($summarizedApiUrl)
+    {
+        $this->summarizedApiUrl = $summarizedApiUrl;
+    }
 
 
 }
