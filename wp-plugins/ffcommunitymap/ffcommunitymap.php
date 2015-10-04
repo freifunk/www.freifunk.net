2ad6acf18c76e0286086b581e3be8ff093ecc02f
andibraeu <ab@andi95.de>
2015-10-04 17:02:59 +0200

    wp-plugins: added plugin to display community map via shortcode

---
+++ wp-plugins/ffcommunitymap/ffcommunitymap.php
@@ -0,0 +1,141 @@
+<?php
+
+/*
+Plugin Name: Ffcommunitymap
+Plugin URI: http://api.freifunk.net
+Description: display the freifunk community map
+Version: 1.0
+Author: Andi Bräu
+Author URI: https://blog.andi95.de
+License: CC0
+*/
+
+function ffcommunitymap($atts)
+{
+    $a = shortcode_atts(array(
+        'feed_url' => '//api.freifunk.net/feed/feed.php',
+        'geojsonurl'=> '//api.freifunk.net/map/ffGeoJsonp.php?callback=?',
+        'mapboxid' => 'mapbox.streets',
+        'showevents' => '1',
+        'shownews' => '0',
+        'hidelocationbutton' => '0',
+        'hidelayercontrol' => '0',
+        'hideinfobox' => '0',
+        'newscontentlimit' => '3',
+        'eventscontentlimit' => '2',
+        'postcontentlength' => '30',
+        'zoomlevel' => '5',
+        'center' => '51.5,10.5',
+        'height' => null,
+        'width' => null
+    ), $atts);
+
+    $feedUrl = esc_url($a['feed_url']);
+    $geoJsonUrl = esc_url($a['geojsonurl']);
+    $mapboxId = esc_html($a['mapboxid']);
+    $showEvents = (esc_js($a['showevents']) === "1") ? "true" : "false";
+    $showNews = (esc_js($a['shownews']) === "1") ? "true" : "false";
+    $hideLocationButton = (esc_js($a['hidelocationbutton']) === "1") ? "true" : "false";
+    $hideLayerControl = (esc_js($a['hidelayercontrol']) === "1") ? "true" : "false";
+    $hideInfoBox = (esc_js($a['hideinfobox']) === "1") ? "true" : "false";
+    $newsContentLimit = is_numeric($a['newscontentlimit']) ? $a['newscontentlimit'] : 3;
+    $eventsContentLimit = is_numeric($a['eventscontentlimit']) ? $a['eventscontentlimit'] : 2;
+    $postContentLength = is_numeric($a['postcontentlength']) ? $a['postcontentlength'] : 30;
+    $zoomLevel = is_numeric($a['zoomlevel']) ? $a['zoomlevel'] : 4;
+    $center = preg_match("/^\d[0-9\.]{1,},\d[0-9\.]{1,}$/", $a['center']) === 1 ? $a['center'] : "51.5,10.5";
+    $height = preg_match("/^\d+(px|%)$/", $a['height']) === 1 ? $a['height'] : null;
+    $width = preg_match("/^\d+(px|%)$/", $a['width']) === 1 ? $a['width'] : null;
+
+    if ( $height ) {
+        $height = 'height: '.$height.';';
+    }
+    if ( $width ) {
+        $width = 'width:' . $width.';';
+    }
+    $style = 'style="' . $height . ' ' . $width . '"';
+
+    $divid = uniqid("map");
+    $output = '<div id="'.$divid.'" class="mapfull" '.$style.'>'.PHP_EOL;
+    $output .= '</div>'.PHP_EOL;
+    $output .= '<script type="text/template" class="template" id="community-popup">';
+    $output .= '<div class="community-popup" data-id="<%- props.shortname %>">'.PHP_EOL;
+    $output .= '<% if ( props.name ) { %>'.PHP_EOL;
+    $output .= '<h2><a href="<%- props.url %>" target="_window"><%- props.name %></a></h2>'.PHP_EOL;
+    $output .= '<% } %>'.PHP_EOL;
+    $output .= '<% if (props.metacommunity) { %>'.PHP_EOL;
+    $output .= '<h3><%- props.metacommunity %></h3>'.PHP_EOL;
+    $output .= '<% } %>'.PHP_EOL;
+    $output .= '<% if (props.city) { %>'.PHP_EOL;
+    $output .= '<div class="city"><%- props.city  %></div>'.PHP_EOL;
+    $output .= '<% } %>'.PHP_EOL;
+    $output .= '<% if (props.nodes) { %>'.PHP_EOL;
+    $output .= '<div class="nodes">Zug&auml;nge: <%- props.nodes  %>'.PHP_EOL;
+    $output .= '<% if (props.state && props.age) { %>'.PHP_EOL;
+    $output .= '<span class="state <%- props.state  %>" title="Die letzte Aktualisierung der Daten war vor <%- props.age  %> Tagen">(<%- props.state  %>)</span>'.PHP_EOL;
+    $output .= ' <% } %>'.PHP_EOL;
+    $output .= '</div>'.PHP_EOL;
+    $output .= '<% } %>'.PHP_EOL;
+    $output .= '<% if (props.phone) { %>'.PHP_EOL;
+    $output .= '<div class="phone">&#9742; <%- props.phone  %></div>'.PHP_EOL;
+    $output .= '<% } %>'.PHP_EOL;
+    $output .= '<ul class="contacts" style="height:<%- Math.round(props.contacts.length/6+0.4)*30+10 %>px; width: <%- 6*(30+5)%>px;">'.PHP_EOL;
+    $output .= '<% _.each(props.contacts, function(contact, index, list) { %>'.PHP_EOL;
+    $output .= '<li class="contact">'.PHP_EOL;
+    $output .= '<a href="<%- contact.url %>" class="button <%- contact.type %>" target="_window"></a>'.PHP_EOL;
+    $output .= '</li>'.PHP_EOL;
+    $output .= '<% }); %>'.PHP_EOL;
+    $output .= '</ul>'.PHP_EOL;
+    $output .= '<div class="events">'.PHP_EOL;
+    $output .= '</div>'.PHP_EOL;
+    $output .= '</div>'.PHP_EOL;
+    $output .= '</script>'.PHP_EOL;
+
+    $output .= '<script>'.PHP_EOL;
+    $output .= 'jQuery.getJSON("'. plugin_dir_url( __FILE__ )  .'config.json", function() {'.PHP_EOL;
+    $output .= 'var widget = FFCommunityMapWidget({'.PHP_EOL;
+    $output .= 'ffGeoJsonUrl: "'.$geoJsonUrl.'",'.PHP_EOL;
+    $output .= 'showEvents: '.$showEvents.','.PHP_EOL;
+    $output .= 'showNews: '.$showNews.','.PHP_EOL;
+    $output .= 'hideLocationButton: '. $hideLocationButton .','.PHP_EOL;
+    $output .= 'hideLayerControl: '. $hideLayerControl .','.PHP_EOL;
+    $output .= 'hideInfoBox: '. $hideInfoBox .','.PHP_EOL;
+    $output .= 'feedUrl: "'.$feedUrl.'",'.PHP_EOL;
+    $output .= 'newsContentLimit: '. $newsContentLimit .','.PHP_EOL;
+    $output .= 'eventsContentLimit: '. $eventsContentLimit .','.PHP_EOL;
+    $output .= 'postContentLength: '. $postContentLength .','.PHP_EOL;
+    $output .= 'zoomLevel: '.$zoomLevel.','.PHP_EOL;
+    $output .= 'center: ['.$center.'],'.PHP_EOL;
+    $output .= 'divid: "'. $divid .'",'.PHP_EOL;
+    $output .= 'mapboxId: "'.$mapboxId . '"'.PHP_EOL;
+    $output .= '});'.PHP_EOL;
+    $output .= '});'.PHP_EOL;
+    $output .= ' </script>'.PHP_EOL;
+
+    return $output;
+
+}
+
+function our_scripts() {
+    wp_enqueue_style("cssleaflet", "//api.freifunk.net/map/external/leaflet/leaflet.css");
+    wp_enqueue_style("cssleafletmc", "//api.freifunk.net/map/external/leaflet/MarkerCluster.css");
+    wp_enqueue_style("cssleafletmcd", "//api.freifunk.net/map/external/leaflet/MarkerCluster.Default.css");
+    wp_enqueue_style("cssleafletbutton", "//api.freifunk.net/map/external/leaflet/leaflet-button-control.css");
+    wp_enqueue_style("cssscrollbar", "//api.freifunk.net/timeline/malihu-scrollbar/jquery.mCustomScrollbar.min.css");
+    wp_enqueue_style("csstimeline", "//api.freifunk.net/timeline/timeline.css");
+    wp_enqueue_style("csstlcustom", "//api.freifunk.net/timeline/custom.css");
+    wp_enqueue_style("csscommunitymap", "//api.freifunk.net/map/community_map.css");
+    wp_enqueue_style("mystyles", plugin_dir_url( __FILE__ ). "ffcommunitymap.css");
+    wp_enqueue_script("leaflet", "//api.freifunk.net/map/external/leaflet/leaflet.js");
+    wp_enqueue_script("leaflet-button-control", "//api.freifunk.net/map/external/leaflet/leaflet-button-control.js");
+    wp_enqueue_script("leafletmc", "//api.freifunk.net/map/external/leaflet/leaflet.markercluster.js");
+    wp_enqueue_script("underscore", "//api.freifunk.net/map/external/underscore/underscore.js");
+    wp_enqueue_script("communitymap", "//api.freifunk.net/map/community_map.js");
+    wp_enqueue_script("scrollbar", "//api.freifunk.net/timeline/malihu-scrollbar/jquery.mCustomScrollbar.concat.min.js");
+    wp_enqueue_script("timeline", "//api.freifunk.net/timeline/timeline.js");
+}
+
+add_action("wp_enqueue_scripts", "our_scripts");
+
+add_shortcode("ffcommunitymap", "ffcommunitymap");
+
+?>
